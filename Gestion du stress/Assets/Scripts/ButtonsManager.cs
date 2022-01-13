0c0dd0d2cbf02fc46e3b84248c0be931bc72d378
clevazquez <clement.vaz30@gmail.com>
2022-01-13 18:24:08 +0100

    Ajout du style graphique du menu

--- Gestion du stress/Assets/Scripts/ButtonsManager.cs
+++ Gestion du stress/Assets/Scripts/ButtonsManager.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ButtonsManager : MonoBehaviour
 {
+    public Animator transition;
     public void Quitter()
     {
         Application.Quit();
     }
-    public void LoadLevel(string levelName)
+    public void LoadNextLevel(string levelName)
     {
+       StartCoroutine(LoadLevel(levelName));
+    }
+
+    IEnumerator LoadLevel(string levelName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
         SceneManager.LoadScene(levelName);
     }
 }
